f06f985032ed4fdd4c193f153c3b8224feb1e5df
ptpofficialxd <aekkungzaza@gmail.com>
2023-03-28 02:50:31 +0700

    update public css/js

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,22 @@
-/* jQuery */
-$(document).ready(function() {
-  $("form").submit(function(event) {
-    event.preventDefault();
-    // Declaration linking to HTML input
-    const email = $('input[id="email"]').val();
-    const password = $('input[id="password"]').val();
-    // Check if email and password are entered
-    if (!email || !password) {
-      alert("You haven't entered your login details. \nTo gain access to the application, please enter your login information!");
-      // Check if email and password are corrected
-    } else if (email === "ptpofficialxd@gmail.com" && password === "aekkung753951") {
-      alert("You have successfully logged in. Welcome to 1G Food Application!");
-      location.href = "home";
-    } else {
-      alert("The Email or password you entered is incorrect. Please try again!");
-    }
-  });
-});
+// /* jQuery */
+// $(document).ready(function() {
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//     // Declaration linking to HTML input
+//     const email = $('input[id="email"]').val();
+//     const password = $('input[id="password"]').val();
+//     // Check if email and password are entered
+//     if (!email || !password) {
+//       alert("You haven't entered your login details. \nTo gain access to the application, please enter your login information!");
+//       // Check if email and password are corrected
+//     } else if (email === "ptpofficialxd@gmail.com" && password === "aekkung753951") {
+//       alert("You have successfully logged in. Welcome to 1G Food Application!");
+//       location.href = "home";
+//     } else {
+//       alert("The Email or password you entered is incorrect. Please try again!");
+//     }
+//   });
+// });
 
 
 /* JavaScript */
---
+++ public/js/signup-success.js
@@ -0,0 +1,4 @@
+const contBtn = document.getElementById("contBtn");
+contBtn.addEventListener("click", function() {
+  location.href = "login";
+});
