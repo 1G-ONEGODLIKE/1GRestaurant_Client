fdde1e621b6dac56966c1efa6adef4951341885d
ptpofficialxd <aekkungzaza@gmail.com>
2023-03-30 04:28:59 +0700

    edit signup success page

--- public/js/signup-success.js
+++ public/js/signup-success.js
@@ -2,3 +2,8 @@ const contBtn = document.getElementById("contBtn");
 contBtn.addEventListener("click", function() {
   location.href = "login";
 });
+
+const contBtn2 = document.getElementById("contBtn2");
+contBtn2.addEventListener("click", function() {
+  location.href = "address";
+});
